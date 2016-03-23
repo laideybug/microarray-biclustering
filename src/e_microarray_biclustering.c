24fbe7d136933ddd0633923c6e7296dcd4068c29
Stephen Laide <stephenlaide@gmail.com>
2016-03-23 23:59:01 +0000

    Minor optimisations made. Changed DMA approach to 'write to' instead of 'read from' other eCores.

--- src/e_microarray_biclustering.c
+++ src/e_microarray_biclustering.c
@@ -11,19 +11,27 @@ float sign(float value);
 void sync_isr(int);
 
 int main(void) {
-	unsigned *xt, *wk, *update_wk, *nu_opt, *nu_k0, *nu_k1, *nu_k2, *done_flag, *src, *dest, *p;
-	unsigned slave_core, j, k, dest_addr;
+	unsigned *xt, *wk, *update_wk, *nu_opt, *nu_k, *nu_k0, *nu_k1, *nu_k2, *done_flag, *dest, *p;
+	unsigned slave_core, j, k, src_addr;
 	int i, reps;
-	float subgrad[IN_ROWS], scaling;
+	float subgrad[IN_ROWS], scaling, rms_wk, rms_wk_reciprocol;
 
-	xt = (unsigned *) XT_MEM_ADDR;	        // Address of xt (56 x 1)
-	wk = (unsigned *) WK_MEM_ADDR;	        // Address of dictionary atom (56 x 1)
+	xt = (unsigned *) XT_MEM_ADDR;	            // Address of xt (56 x 1)
+	wk = (unsigned *) WK_MEM_ADDR;	            // Address of dictionary atom (56 x 1)
 	update_wk = (unsigned *) UP_WK_MEM_ADDR;	// Address of update atom (56 x 1)
-	nu_opt = (unsigned *) NU_OPT_MEM_ADDR;       // Address of optimal dual variable (56 x 1)
-	nu_k0 = (unsigned *) NU_K0_MEM_ADDR;	    // Address of this cores dual variable estimate (56 x 1)
-	nu_k1 = (unsigned *) NU_K1_MEM_ADDR;        // Address of neighbour 1 dual variable estimate (56 x 1)
-	nu_k2 = (unsigned *) NU_K2_MEM_ADDR;        // Address of neighbour 2 dual variable estimate (56 x 1)
-	done_flag = (unsigned *) DONE_MEM_ADDR;	// "Done" flag (1 x 1)
+	nu_opt = (unsigned *) NU_OPT_MEM_ADDR;      // Address of optimal dual variable (56 x 1)
+	nu_k0 = (unsigned *) NU_K0_MEM_ADDR;	    // Address of node 0 dual variable estimate (56 x 1)
+	nu_k1 = (unsigned *) NU_K1_MEM_ADDR;        // Address of node 1 dual variable estimate (56 x 1)
+	nu_k2 = (unsigned *) NU_K2_MEM_ADDR;        // Address of node 2 dual variable estimate (56 x 1)
+	done_flag = (unsigned *) DONE_MEM_ADDR;	    // "Done" flag (1 x 1)
+
+    src_addr = (unsigned)NU_K0_MEM_ADDR;
+
+    for (i = 0; i < e_group_config.core_col; ++i) {
+        src_addr = src_addr + (unsigned)NU_MEM_OFFSET;
+    }
+
+    nu_k = (unsigned *)src_addr;    // Address of this cores dual variable estimate
     p = 0x0000;
 
     // Re-enable interrupts
@@ -50,23 +58,16 @@ int main(void) {
 
 			for (i = 0; i < IN_ROWS; ++i) {
 				/* D * diagmat(scaling*my_minus_mu) */
-				nu_k0[i] = wk[i] * (scaling * -MU_2);
+				nu_k[i] = wk[i] * (scaling * -MU_2);
 			}
-
-			// Synch with all other cores
-			e_barrier(barriers, tgt_bars);
-
-			dest_addr = (unsigned)NU_K0_MEM_ADDR;
 
 	        // Exchange dual variable estimates
 			for (j = 0; j < e_group_config.group_rows; ++j) {
 	            for (k = 0; k < e_group_config.group_cols; ++k) {
 	                if ((j != e_group_config.core_row) | (k != e_group_config.core_col)) {
-	                    slave_core = (unsigned)e_get_global_address(j, k, p);
-	                    src = (unsigned *)(slave_core + (unsigned)NU_K0_MEM_ADDR);
-	                    dest_addr = dest_addr + (unsigned)NU_MEM_OFFSET;
-	                    dest = (unsigned *)(dest_addr);
-	                    e_dma_copy(dest, src, IN_ROWS*sizeof(float));
+                        slave_core = (unsigned)e_get_global_address(i, j, p);
+	                    dest = (unsigned *)(slave_core + (unsigned)src_addr);
+	                    e_dma_copy(dest, nu_k, IN_ROWS*sizeof(float));
 	                }
 	            }
 			}
@@ -76,7 +77,7 @@ int main(void) {
 
 	    	// Average dual variable estimates
 			for (i = 0; i < IN_ROWS; ++i) {
-	            nu_opt[i] = subgrad[i] + ((nu_k0[i] + nu_k1[i] + nu_k2[i]) * ONE_OVER_N);
+	            nu_opt[i] = nu_opt[i] + subgrad[i] + ((nu_k0[i] + nu_k1[i] + nu_k2[i]) * ONE_OVER_N);
 			}
 		}
 
@@ -88,7 +89,7 @@ int main(void) {
 		adjustScaling(scaling);
 
 		// Update dictionary atom
-		float rms_wk = 0.0f;
+		rms_wk = 0.0f;
 
 		// Create update atom (Y_opt)
 		for (i = 0; i < IN_ROWS; ++i) {
@@ -105,7 +106,7 @@ int main(void) {
 		rms_wk = sqrt(rms_wk);
 
 		if (rms_wk > 1.0f) {
-            float rms_wk_reciprocol = 1.0f / rms_wk;
+            rms_wk_reciprocol = 1.0f / rms_wk;
 
 			for (i = 0; i < IN_ROWS; ++i) {
 				wk[i] = wk[i] * rms_wk_reciprocol;
