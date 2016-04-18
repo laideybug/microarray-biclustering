6d29cd588af1824a188cadcf0db8c6860cced1ca
Stephen Laide <stephenlaide@gmail.com>
2016-04-18 00:32:53 +0000

    Changed pow function calls to expect floats.

--- libs/mb-utils/mb_norm.c
+++ libs/mb-utils/mb_norm.c
@@ -5,7 +5,7 @@ float mb_norm(size_t length, float vect[length]) {
     float v_sum = 0.0f;
 
     for (int i = 0; i < length; ++i) {
-        v_sum += pow(vect[i], 2);
+        v_sum += powf(vect[i], 2);
     }
 
     return sqrtf(v_sum);
--- libs/mb-utils/mb_square.c
+++ libs/mb-utils/mb_square.c
@@ -5,7 +5,7 @@ void mb_square(size_t rows, size_t cols, float matrix[rows][cols]) {
     for (int j = 0; j < rows; ++j) {
         for (int k = 0; k < cols; ++k) {
             float element = matrix[j][k];
-            matrix[j][k] = pow(element, 2);
+            matrix[j][k] = powf(element, 2);
         }
     }
 }
